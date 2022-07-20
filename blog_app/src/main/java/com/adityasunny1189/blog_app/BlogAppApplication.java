731ffeab76df4c3fffc365ccfe6e582451c394d4
Aditya Pathak <adityapathak@192.168.1.3>
2022-07-20 13:34:00 +0530

    initial commit

---
+++ blog_app/src/main/java/com/adityasunny1189/blog_app/BlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.adityasunny1189.blog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApplication.class, args);
+	}
+
+}
---
+++ blog_app/src/test/java/com/adityasunny1189/blog_app/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityasunny1189.blog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
